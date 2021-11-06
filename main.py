9eb90b121550cd5546e829e0413b76d6b893f83a
Cole Amlong <git config --global user.email coleamlong@gmail.comgit config --global user.email>
2021-11-05 20:55:05 -0500

    restructure

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ WIDTH, HEIGHT = 800, 800
 BOARD_COLORS = ["#444444", "#aaaaaa"]
 SQUARE_SIZE = 100
 
+
 class Root(Tk):
-    def __init__(self, boardSize = 8) -> None:
+    def __init__(self, boardSize=8) -> None:
         super(Root, self).__init__()
         self.boardSize = boardSize
         self.title("ChessAI")
@@ -16,10 +17,11 @@ class Root(Tk):
         self.wm_iconbitmap('window_icon.ico')
         self.minsize(WIDTH, HEIGHT)
 
-        self.canvas = tkinter.Canvas(self, bg="white", width=WIDTH, height=HEIGHT)
+        self.canvas = tkinter.Canvas(
+            self, bg="white", width=WIDTH, height=HEIGHT)
         self.canvas.grid(row=0, column=1, columnspan=8, rowspan=8)
-        self.board = [[None for row in range(boardSize)] for col in range(boardSize)]
-
+        self.board = [[None for row in range(boardSize)]
+                      for col in range(boardSize)]
 
     def init_board(self):
         from itertools import cycle
@@ -29,7 +31,9 @@ class Root(Tk):
             color = cycle(BOARD_COLORS[::-1] if not col % 2 else BOARD_COLORS)
             for row in range(self.boardSize):
 
-                self.board[row][col] = SquareButton(self.canvas, row, col, SQUARE_SIZE, next(color))
+                self.board[row][col] = SquareButton(
+                    self.canvas, row, col, SQUARE_SIZE, next(color))
+
 
 class SquareButton:
     def __init__(self, canvas: tkinter.Canvas, row: int, column: int, size: int, color: str) -> None:
@@ -42,13 +46,21 @@ class SquareButton:
         y1 = self.column * self.size
         x2 = x1 + self.size
         y2 = y1 + self.size
-        canvas.create_rectangle(x1, y1, x2, y2, fill=self.color, tags=f"tile{self.column}{self.row}", outline="")
-        canvas.tag_bind(f"tile{self.column}{self.row}", "<Button-1>", self.__str__)
+        canvas.create_rectangle(
+            x1, y1, x2, y2, fill=self.color, tags=f"tile{self.column}{self.row}", outline="")
+        canvas.tag_bind(f"tile{self.column}{self.row}",
+                        "<Button-1>", self.__str__)
 
     def __str__(self, event=None):
-        print(f"{chess.FILE_NAMES[self.row]}{chess.RANK_NAMES[7 - self.column]}")
+        print(
+            f"{chess.FILE_NAMES[self.row]}{chess.RANK_NAMES[7 - self.column]}")
 
 
-root = Root()
-root.init_board()
-root.mainloop()
+# root = Root()
+# root.init_board()
+# root.mainloop()
+
+board = chess.Board()
+print(board)
+board.push_uci("e2e4")
+print(board)
---
+++ network/activation_functions.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+
+# binary step function
+def bin_step(x):
+    raise NotImplementedError
+
+
+def bin_step_prime(x):
+    raise NotImplementedError
+
+
+# binary step function
+def bin_step(x):
+    raise NotImplementedError
+
+
+def bin_step_prime(x):
+    raise NotImplementedError
+
+
+# binary step function
+def bin_step(x):
+    raise NotImplementedError
+
+
+def bin_step_prime(x):
+    raise NotImplementedError
+
+
+# binary step function
+def bin_step(x):
+    raise NotImplementedError
+
+
+def bin_step_prime(x):
+    raise NotImplementedError
+
+
+# binary step function
+def bin_step(x):
+    raise NotImplementedError
+
+
+def bin_step_prime(x):
+    raise NotImplementedError
+
+
+# binary step function
+def bin_step(x):
+    raise NotImplementedError
+
+
+def bin_step_prime(x):
+    raise NotImplementedError
